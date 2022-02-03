a2af7e6c5603e9063ebb7db86cf2934d8966813e
TMIYABE <taishi.miyabe@diva.co.jp>
2022-02-03 23:47:28 +0900

    A-NotOverflow
    A-NotOverflow

---
+++ A-Not Overflow.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoderPractice
+{
+    class NotOverFlow
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var input = Console.ReadLine();
+                var isNumber = new ValidateNum().isNumber(input);
+                if (!isNumber)
+                {
+                    throw new FormatException();
+                }
+
+                var num = int.Parse(input);
+                Console.WriteLine("Yes");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("\nMessage ---\n{0}", ex.Message);
+                Console.WriteLine("No");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("\nMessage ---\n{0}", ex.Message);
+                Console.WriteLine("No");
+            }
+        }
+    }
+
+    class ValidateNum
+    {
+        public bool isNumber(string num_str)
+        {
+            if (string.IsNullOrEmpty(num_str))
+            {
+                return false;
+            }
+            var headChar = num_str.Substring(0, 1);
+            num_str = (headChar == "-") ? num_str.Substring(1) : num_str;
+            return (num_str.All(char.IsDigit));
+        }
+    }
+}
