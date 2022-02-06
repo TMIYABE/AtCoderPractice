b3436454ab7a8f5d3557a64a96f4ebff3c7ed209
TMIYABE <taishi.miyabe@diva.co.jp>
2022-02-07 00:28:58 +0900

    レビュー指摘検討
    ４：B問題：SetUpというクラス名はちょっと理解しづらいですね。SetUpとOutPutそれぞれのクラスの責務を書きだしてください。
    
    ５：B問題：Setupクラスですが、setupmatrixにmatrixの参照を渡して内部で直接書き換えるのは行儀が悪くかつ、testableなmethodではありません。
    
    　新しくmetho内部でint[,]を作成し、voidではなく、int[,]を戻すべきです。
    
    私なら、setupとoutputを一つのクラスにし、setupmatrixをコンストラクタに変え、引数のmatrixをconvertしたものをフィールドにもち、invertはprivateでコンストラクタから呼び、outputoはpublicのままにします。

---
+++ 237/B - Matrix Transposition.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoderPractice
+{
+    //https://atcoder.jp/contests/abc237/tasks/abc237_b
+    class B___Matrix_Transposition
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToList();
+                if (input.Count() != 2)
+                {
+                    throw new FormatException("数字を2つ、半角スペースで区切って入力してください");
+                }
+                var H = input[0];
+                var W = input[1];
+
+                var mrx = new Matrix(H, W);
+                var matrix = mrx.matrix;
+                var matrix_inverted = mrx.matrix_inverted;
+
+                mrx.outputMatrixToConsole(matrix_inverted);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("\nMessage ---\n{0}", ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("\nMessage ---\n{0}", ex.Message);
+            }
+        }
+    }
+
+    class Matrix
+    {
+        public int[,] matrix;
+        public int[,] matrix_inverted;
+
+        public Matrix(int Height, int Width)
+        {
+            matrix = SetupMatrix(Height, Width);
+            matrix_inverted = InvertMatrix(matrix);
+        }
+
+        private int[,] SetupMatrix(int Height, int Width)
+        {
+            var ret = new int[Height, Width];
+
+            for (int i = 0; i < Height; i++)
+            {
+                var matrixRow = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToList();
+
+                for (int j = 0; j < Width; j++)
+                {
+                    ret[i, j] = matrixRow[j];
+                }
+            }
+            return ret;
+        }
+        private int[,] InvertMatrix(int[,] matrix)
+        {
+            var argHeight = matrix.GetLength(0);
+            var argWidth = matrix.GetLength(1);
+
+            var ret = new int[argWidth, argHeight];
+
+            for (int i = 0; i < argWidth; i++)
+            {
+                for (int j = 0; j < argHeight; j++)
+                {
+                    ret[i, j] = matrix[j, i];
+                }
+            }
+
+            return ret;
+        }
+
+        public void outputMatrixToConsole(int[,] matrix)
+        {
+            var argHeight = matrix.GetLength(0);
+            var argWidth = matrix.GetLength(1);
+
+            for (int i = 0; i < argHeight; i++)
+            {
+                for (int j = 0; j < argWidth; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
