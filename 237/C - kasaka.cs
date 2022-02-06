70d4cd528f8f092bfe472ff37c9a43ef5cf7c6b8
TMIYABE <taishi.miyabe@diva.co.jp>
2022-02-07 00:29:11 +0900

    レビュー指摘検討
    ６：C問題：checkではなく、checkerまたはcheckhelperが妥当です。動詞をクラス名にするべきではありません。
    
    ７：C問題：CheckInputとCheckReversible_Byを分けている理由は何ですか？
    
    ８：C問題：70行目から79行目、LINQで書けると思います。
    
    ９：C問題：65行目、stringのrefは使ってはいけません。副作用のあるmethod　という単語について調べてみてください。

---
+++ 237/C - kasaka.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoderPractice
+{
+    //https://atcoder.jp/contests/abc237/tasks/abc237_c
+    class C___kasaka
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var input = Console.ReadLine();
+                var checker = new Checker();
+                checker.CheckInput(input);
+
+                if (checker.CheckReversible_ByAdding_a(input))
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("\nMessage ---\n{0}", ex.Message);
+                Console.WriteLine("No");
+            }
+        }
+    }
+
+    class Checker
+    {
+        public void CheckInput(string input)
+        {
+            if (input.Count() == 0 || !input.All(Char.IsLower))
+            {
+                throw new FormatException("小文字のみの文字列を、1文字以上入力してください。");
+            }
+        }
+
+        //先頭にaを加えることで反転可能になるかどうかのチェック
+        public bool CheckReversible_ByAdding_a(string input)
+        {
+            string input_without_a;
+            char a = 'a';
+            if (input.All(x => x == a)) return true;
+
+            var a_count_front = Count_a(input);
+
+            var input_reverse = String.Join("", input.Reverse());
+            var a_count_back = Count_a(input_reverse);
+
+            if (a_count_front > a_count_back) return false;
+
+            input_without_a = input.Substring(a_count_front, input.Length - a_count_front - a_count_back);
+            return CheckReversible(input_without_a.ToArray());
+
+
+        }
+
+        private int Count_a(string str)
+        {
+            char a = 'a';
+            var a_count = str.TakeWhile(x => x == a).Count();
+
+            return a_count;
+        }
+
+        private bool CheckReversible(char[] arr)
+        {
+            var arrClone = new String(arr);
+            return arrClone.Reverse().ToArray().SequenceEqual(arr);
+        }
+    }
+}
